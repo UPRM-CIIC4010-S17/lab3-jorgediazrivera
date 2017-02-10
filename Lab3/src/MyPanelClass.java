2e38d7e240d9e3a4c8b942f06a239c341c41337a
Jorge <Jorge@JorgeDiaz>
2017-02-10 17:33:42 -0400

    Commented out the oval and added a polygon

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -1,6 +1,8 @@
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Insets;
+import java.awt.Polygon;
+
 import javax.swing.JPanel;
  
 public class MyPanelClass extends JPanel 
@@ -39,9 +41,19 @@ public class MyPanelClass extends JPanel
 //                        g.setColor(Color.ORANGE);
 //                        g.drawLine(x1, y2, x2, y1);
                         
-                        g.setColor(Color.LIGHT_GRAY);
-                        g.fillOval(width/2-27, height/2-27, 55, 55);
+//                        g.setColor(Color.LIGHT_GRAY);
+//                        g.fillOval(width/2-27, height/2-27, 55, 55);
                         
+                          Polygon p = new Polygon();
+                          p.addPoint(x1 + 5, y1 + 25);
+                          p.addPoint(x1 + 20, y1 + 10);
+                          p.addPoint(x1 + 35, y1 + 25);
+                          p.addPoint(x1 + 25, y1 + 25);
+                          p.addPoint(x1 + 25, y1 + 45);
+                          p.addPoint(x1 + 15, y1 + 45);
+                          p.addPoint(x1 + 15, y1 + 25);
+                          g.setColor(Color.YELLOW);
+                          g.fillPolygon(p);
                                          
             }
 }
