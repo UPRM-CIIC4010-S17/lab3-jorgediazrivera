f9274b7df3ff39ba493e752845aebbf827078137
Jorge <Jorge@JorgeDiaz>
2017-02-10 21:45:31 -0400

    Removed every commented part and added the flag

--- Lab3/src/MyPanelClass.java
+++ Lab3/src/MyPanelClass.java
@@ -26,34 +26,64 @@ public class MyPanelClass extends JPanel
                         int height = y2 - y1;
  
                         //Paint the background
-                        g.setColor(Color.DARK_GRAY);
+                        g.setColor(Color.WHITE);
                         g.fillRect(x1, y1, width+1, height+1);
                         
-                        //Draw a border
-//                        g.setColor(Color.RED);
-//                        g.drawRect(x1, y1+2, width, height-2);
-//                        g.setColor(Color.YELLOW);
-//                        g.drawRect(x1+5, y1+7, width-10, height-14);
-//                        
-//                        g.setColor(Color.WHITE);
-//                        g.drawLine(x1, y1, x2, y2);
-//                        
-//                        g.setColor(Color.ORANGE);
-//                        g.drawLine(x1, y2, x2, y1);
+                        //Draws the border of the flag, stripes, triangle and star
+                        g.setColor(Color.RED);
+                        g.drawRect(x1+10, y1+10, x1+210, y1+150);
                         
-//                        g.setColor(Color.LIGHT_GRAY);
-//                        g.fillOval(width/2-27, height/2-27, 55, 55);
+                        Polygon stripe1 = new Polygon();
+                        stripe1.addPoint(x1+10, y1+10);
+                        stripe1.addPoint(x1+220, y1+10);
+                        stripe1.addPoint(x1+220, y1+40);
+                        stripe1.addPoint(x1+10, y1+40);
+                        g.setColor(Color.RED);
+                        g.fillPolygon(stripe1);
                         
-                          Polygon p = new Polygon();
-                          p.addPoint(x1 + 5, y1 + 25);
-                          p.addPoint(x1 + 20, y1 + 10);
-                          p.addPoint(x1 + 35, y1 + 25);
-                          p.addPoint(x1 + 25, y1 + 25);
-                          p.addPoint(x1 + 25, y1 + 45);
-                          p.addPoint(x1 + 15, y1 + 45);
-                          p.addPoint(x1 + 15, y1 + 25);
-                          g.setColor(Color.YELLOW);
-                          g.fillPolygon(p);
+                        
+                        Polygon stripe2 = new Polygon();
+                        stripe2.addPoint(x1+10, y1+70);
+                        stripe2.addPoint(x1+220, y1+70);
+                        stripe2.addPoint(x1+220, y1+100);
+                        stripe2.addPoint(x1+10, y1+100);
+                        g.setColor(Color.RED);
+                        g.fillPolygon(stripe2);                 
+                        
+                        Polygon stripe3 = new Polygon();
+                        stripe3.addPoint(x1+10, y1+130);
+                        stripe3.addPoint(x1+220, y1+130);
+                        stripe3.addPoint(x1+220, y1+160);
+                        stripe3.addPoint(x1+10, y1+160);
+                        g.setColor(Color.RED);
+                        g.fillPolygon(stripe3);  
+                        
+                       
+                        
+                        Polygon triangle = new Polygon();
+                        triangle.addPoint(x1+10, y1+10);
+                        triangle.addPoint(x1+10, y1+160);
+                        triangle.addPoint(x1+115, y1+85);
+                        g.setColor(Color.BLUE);
+                        g.fillPolygon(triangle);
+                        
+                        Polygon p2 = new Polygon();
+                        p2.addPoint(x1 + 25, y1 + 73);
+                        p2.addPoint(x1 + 41, y1 + 73);
+                        p2.addPoint(x1 + 47, y1 + 58);
+                        p2.addPoint(x1 + 53, y1 + 73);
+                        p2.addPoint(x1 + 69, y1 + 73);
+                        p2.addPoint(x1 + 56, y1 + 83);
+                        p2.addPoint(x1 + 61, y1 + 98);
+                        p2.addPoint(x1 + 47, y1 + 88);
+                        p2.addPoint(x1 + 34, y1 + 98);
+                        p2.addPoint(x1 + 38, y1 + 83);
+                        g.setColor(Color.WHITE);
+                        g.fillPolygon(p2);
+                        
+                        
+                  
+                        
                                          
             }
 }
